66bfb068a8552d4d953fdce1e9f3d9311da134ea
Olivier Lefebvre <lefebvre.olivier11@gmail.com>
2020-05-06 16:26:13 +1000

    Utilised new getLength function in tests

--- src/test/java/org/lefmaroli/random/RandomGeneratorTest.java
+++ src/test/java/org/lefmaroli/random/RandomGeneratorTest.java
@@ -10,8 +10,6 @@ public class RandomGeneratorTest {
     @Test
     public void getRandomUnitVector2D() {
         Vector2D unitVector = new RandomGenerator().getRandomUnitVector2D();
-        Double x = unitVector.getX();
-        Double y = unitVector.getY();
-        assertEquals(1.0, x * x + y * y, 1E-18);
+        assertEquals(1.0, unitVector.getLength(), 1E-18);
     }
 }
