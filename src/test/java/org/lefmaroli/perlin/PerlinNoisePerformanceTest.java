45920cb7db82c2afcd568844246e36d07ffcc33e
Olivier Lefebvre <lefebvre.olivier11@gmail.com>
2021-05-12 17:41:42 +1000

    Added call to interrupt test if it runs over time limit

--- src/test/java/org/lefmaroli/perlin/PerlinNoisePerformanceTest.java
+++ src/test/java/org/lefmaroli/perlin/PerlinNoisePerformanceTest.java
@@ -7,10 +7,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.awaitility.Awaitility;
-import org.junit.Before;
 import org.junit.Test;
 import org.lefmaroli.factorgenerator.DoubleGenerator;
 import org.lefmaroli.perlin.exceptions.NoiseBuilderException;
@@ -23,43 +23,50 @@ import org.lefmaroli.perlin.slice.SliceNoiseGeneratorBuilder;
 
 public class PerlinNoisePerformanceTest {
 
-  private PerlinNoise perlinNoise;
   Logger logger = LogManager.getLogger(PerlinNoisePerformanceTest.class);
 
-  @Before
-  public void setup() {
-    perlinNoise = new PerlinNoise(1, System.currentTimeMillis());
-  }
-
-  @Test
-  public void testPerformance() {
+  private void testPerformance(
+      int numIterations, Consumer<Integer> c, Duration maxTestDuration, String testTitle) {
     ExecutorService service = Executors.newSingleThreadExecutor();
     AtomicBoolean isDone = new AtomicBoolean(false);
     service.submit(
         () -> {
-          int numIterations = 1000000;
           long start = System.currentTimeMillis();
           for (int i = 0; i < numIterations; i++) {
-            perlinNoise.getFor(i * 0.005);
+            c.accept(i);
+            if (Thread.interrupted()) {
+              return;
+            }
           }
-          logger.info("Done in " + (System.currentTimeMillis() - start) + "ms");
+          logger.info(testTitle + " done in " + (System.currentTimeMillis() - start) + "ms");
           isDone.set(true);
         });
     try {
       Awaitility.setDefaultPollInterval(Duration.ofMillis(10));
-      waitAtMost(Duration.ofMillis(80)).until(isDone::get);
+      waitAtMost(maxTestDuration).until(isDone::get);
     } finally {
-      service.shutdown();
+      service.shutdownNow();
       try {
         boolean areTasksDone = service.awaitTermination(10, TimeUnit.SECONDS);
         if (!areTasksDone) {
           logger.error("Tasks were not completed within the delay");
-        }
+        }else
+          logger.debug("ExecutorService shutdown complete");
       } catch (InterruptedException e) {
         logger.error("Awaiting task termination was interrupted");
         Thread.currentThread().interrupt();
       }
     }
+  }
+
+  @Test
+  public void benchmarkCorePerformance() {
+    PerlinNoise perlinNoise = new PerlinNoise(1, System.currentTimeMillis());
+    testPerformance(
+        1000000,
+        (i) -> perlinNoise.getFor(i * 0.005),
+        Duration.ofMillis(80),
+        "PerlinNoise core benchmark");
   }
 
   @Test
@@ -71,34 +78,8 @@ public class PerlinNoisePerformanceTest {
             .withNoiseStepSizeGenerator(new DoubleGenerator(1.0 / 50, 0.5))
             .withAmplitudeGenerator(new DoubleGenerator(1.0, 0.85))
             .build();
-
-    ExecutorService service = Executors.newSingleThreadExecutor();
-    AtomicBoolean isDone = new AtomicBoolean(false);
-    service.submit(
-        () -> {
-          int numberOfIterations = 50000;
-          long start = System.currentTimeMillis();
-          for (int i = 0; i < numberOfIterations; i++) {
-            noiseGenerator.getNext();
-          }
-          logger.info("Done in " + (System.currentTimeMillis() - start) + "ms");
-          isDone.set(true);
-        });
-    try {
-      Awaitility.setDefaultPollInterval(Duration.ofMillis(10));
-      waitAtMost(Duration.ofMillis(170)).until(isDone::get);
-    } finally {
-      service.shutdown();
-      try {
-        boolean areTasksDone = service.awaitTermination(10, TimeUnit.SECONDS);
-        if (!areTasksDone) {
-          logger.error("Tasks were not completed within the delay");
-        }
-      } catch (InterruptedException e) {
-        logger.error("Awaiting task termination was interrupted");
-        Thread.currentThread().interrupt();
-      }
-    }
+    testPerformance(
+        500, (i) -> noiseGenerator.getNext(), Duration.ofMillis(170), "PointGenerator benchmark");
   }
 
   @Test
@@ -111,34 +92,8 @@ public class PerlinNoisePerformanceTest {
             .withLineStepSizeGenerator(new DoubleGenerator(1.0 / 50, 0.5))
             .withAmplitudeGenerator(new DoubleGenerator(1.0, 0.85))
             .build();
-
-    ExecutorService service = Executors.newSingleThreadExecutor();
-    AtomicBoolean isDone = new AtomicBoolean(false);
-    service.submit(
-        () -> {
-          int numberOfIterations = 500;
-          long start = System.currentTimeMillis();
-          for (int i = 0; i < numberOfIterations; i++) {
-            noiseGenerator.getNext();
-          }
-          logger.info("Done in " + (System.currentTimeMillis() - start) + "ms");
-          isDone.set(true);
-        });
-    try {
-      Awaitility.setDefaultPollInterval(Duration.ofMillis(10));
-      waitAtMost(Duration.ofMillis(700)).until(isDone::get);
-    } finally {
-      service.shutdown();
-      try {
-        boolean areTasksDone = service.awaitTermination(10, TimeUnit.SECONDS);
-        if (!areTasksDone) {
-          logger.error("Tasks were not completed within the delay");
-        }
-      } catch (InterruptedException e) {
-        logger.error("Awaiting task termination was interrupted");
-        Thread.currentThread().interrupt();
-      }
-    }
+    testPerformance(
+        50000, (i) -> noiseGenerator.getNext(), Duration.ofMillis(700), "LineGenerator benchmark");
   }
 
   @Test
@@ -152,33 +107,7 @@ public class PerlinNoisePerformanceTest {
             .withHeightInterpolationPointGenerator(new DoubleGenerator(1.0 / 50, 0.5))
             .withAmplitudeGenerator(new DoubleGenerator(1.0, 0.85))
             .build();
-
-    ExecutorService service = Executors.newSingleThreadExecutor();
-    AtomicBoolean isDone = new AtomicBoolean(false);
-    service.submit(
-        () -> {
-          int numberOfIterations = 5;
-          long start = System.currentTimeMillis();
-          for (int i = 0; i < numberOfIterations; i++) {
-            noiseGenerator.getNext();
-          }
-          logger.info("Done in " + (System.currentTimeMillis() - start) + "ms");
-          isDone.set(true);
-        });
-    try {
-      Awaitility.setDefaultPollInterval(Duration.ofMillis(10));
-      waitAtMost(Duration.ofMillis(2200)).until(isDone::get);
-    } finally {
-      service.shutdown();
-      try {
-        boolean areTasksDone = service.awaitTermination(65, TimeUnit.SECONDS);
-        if (!areTasksDone) {
-          logger.error("Tasks were not completed within the delay");
-        }
-      } catch (InterruptedException e) {
-        logger.error("Awaiting task termination was interrupted");
-        Thread.currentThread().interrupt();
-      }
-    }
+    testPerformance(
+        5, (i) -> noiseGenerator.getNext(), Duration.ofMillis(2200), "SliceGenerator benchmark");
   }
 }
