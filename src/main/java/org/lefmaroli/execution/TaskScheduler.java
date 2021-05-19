ade33f556ec73deb97e5743215699ecdc16b7ac3
Olivier Lefebvre <lefebvre.olivier11@gmail.com>
2021-05-19 21:02:45 +1000

    Cleaned up useless files

--- src/main/java/org/lefmaroli/execution/TaskScheduler.java
+++
@@ -1,8 +0,0 @@
-package org.lefmaroli.execution;
-
-import java.util.concurrent.Callable;
-import java.util.concurrent.CompletableFuture;
-
-public interface TaskScheduler {
-  <R> CompletableFuture<R> schedule(Callable<R> task);
-}
